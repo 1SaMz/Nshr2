7299945a0362a11f58da4280df0d773d78bd0cf0
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-29 16:06:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-﻿const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+const Discord = require('discord.js');
+const client= new Discord.Client();
 
 console.log("BOT ONLINE");
  
-A7MD.on("guildMemberAdd", member => {
+client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
@@ -18,9 +18,4 @@ A7MD.on("guildMemberAdd", member => {
 **`) 
 }).catch(console.error)
 })
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); 
